58a1ab14d7228233316c6ac56ea17051e0d86dba
Ed Bueler <elbueler@alaska.edu>
2014-01-22 20:40:55 -0900

    minor fiddles with fluxlimiter program

--- M615S14/fluxlimiter.m
+++ M615S14/fluxlimiter.m
@@ -1,39 +1,48 @@
 function fluxlimiter(J,nu,k,init)
 % FLUXLIMITER  Solves for u(x,t) at t=k with
 %   u_t + u_x = 0  on  0 < x < 1
-% with periodic boundary conditions and with either square wave
+% with periodic boundary conditions and with either 'square' wave
 % initial shape
 %   u(x,0) = 1  if 0.3 < x < 0.7,
 %            0  otherwise
 % or smoother 'hump' initial shape,
 %   u(x,0) = cos^2(2 pi (x-0.5))  if 0.25 < x < 0.75
 %          = 0                  otherwise.
+% Because velocity is 1 and length of interval is 1, the duration
+% t=k is the number of times the solution "wraps around" the interval.
+%
 % Demonstrates four schemes in subplots in one figure: upwind (top),
 % Lax-Wendroff (next), and a flux-limited, nonlinear (bottom two).
-% The last two subplots differ in their time-integration scheme:
+% The last two subplots differ only in their time-integration scheme:
 % upper is explicit Euler and lower is an order 2 Runge-Kutta method
 % (= modified Euler).
-%    The flux-limited scheme is defined in equations (1.2), (1.3), and
+%
+% The flux-limited scheme is defined in equations (1.2), (1.3), and
 % (1.7) of
+%
 %    Hundsdorfer and Verwer, "Numerical Solution of Time-Dependent
 %    Advection-Diffusion-Reaction Equations", Springer 2003
+%
 % which we call "H&V".  The particular flux-limiter is the kappa=1/3
 % case of (1.10) in H&V, the Koren (1993) choice.
+%
 % Form: fluxlimiter(J,nu,k,init)
+% The first and third arguments J (= number of subintervals) and k
+% (= duration) must be integers.
+%
+% The flux-limited scheme requires nu <= 1/2 with explicit Euler
+% time integration; see (1.12) in H&V (2003).  Note nu = dt / dx.
+%
 % Example:
 %    >> fluxlimiter(50,0.5,1,'square')
 %    >> fluxlimiter(50,0.33,1,'hump')
 %    >> fluxlimiter(100,0.33,10,'square')
 %    >> fluxlimiter(100,0.33,10,'hump')
-% The first and third arguments are J and k which must be integers.
-% The flux-limited scheme requires nu <= 1/2 with explicit Euler
-% time integration; see (1.12) in H&V (2003).
 
 if nargin<1,  J = 50;   end
 if nargin<2,  nu = 0.5;   end
 if nargin<3,  k = 1;   end
 if nargin<4,  init = 'square';   end
-figure(1), clf
 
 % spatial grid
 dx = 1.0 / J;  x  = 0:dx:1-dx;
@@ -67,7 +76,9 @@ elseif all(init=='hump')
   uexact(xfine<0.25) = 0.0;  uexact(xfine>0.75) = 0.0; % smooth hump
 end
 
-% three numerical schemes
+% four numerical schemes
+figure(1), clf
+tstr = {'upwind','Lax-Wendroff','flux limiter (Euler)','flux limiter (RK2)'};
 for fig=1:4
       u = uinit;
       for n=1:NN-1
@@ -81,7 +92,6 @@ for fig=1:4
         else                             % flux-limiter
           theta = gettheta(J,u);
           psi = getpsi(theta);
-          %figure(2), hold on, plot(theta,ppsi,'*'), axis([-3 6 -0.5 1.5])
           fstag = u + psi .* ([u(2:J) u(1)] - u);  % add to upwind version
           uEu = u - nu * (fstag - [fstag(J) fstag(1:J-1)]);  % exp Euler
         end
@@ -95,22 +105,11 @@ for fig=1:4
                     (nu/2) * (fstag2 - [fstag2(J) fstag2(1:J-1)]);
         end
       end
-      figure(1), subplot(4,1,fig)
+      subplot(4,1,fig)
       plot(xfine,uexact,'r')   % exact soln on fine grid
       hold on,  plot(x,u,'o-','markersize',6,'linewidth',1.0)
       hold off, axis([0 1 -0.3 1.3]), axis off
-      if fig==1, title('upwind')
-      elseif fig==2, title('Lax-Wendroff')
-      elseif fig==3, title('flux limiter (Euler)')
-      elseif fig==4, title('flux limiter (RK2)')
-      end
-
-      if fig==3
-        figure(2)
-        plot(xfine,uexact,'r','linewidth',5.0)   % exact soln on fine grid
-        hold on,  plot(x,u,'o-','markersize',6,'linewidth',5.0)
-        hold off, axis([0 1 -0.3 1.3]), axis off
-      end
+      title(tstr{fig})
 end
 
 function th = gettheta(J,u)
