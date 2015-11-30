864139a80aad95b62010af221ce79275ee605e0d
Ed Bueler <elbueler@alaska.edu>
2015-11-30 08:59:42 -0900

    Post code on A10S.

---
+++ M614F15/matlab/geouter.m
@@ -0,0 +1,11 @@
+function [L, U] = geouter(A)
+% GEOUTER  Gauss elimination without pivoting, but in "outer
+% product form".  UNSTABLE: DO NOT USE FOR SERIOUS WORK.
+
+[m n] = size(A);
+if m ~= n, error('only works for square matrices'), end
+U = A;  L = eye(m,m);
+for k = 1:m-1                      % put zeros in these columns
+  L(k+1:m,k) = U(k+1:m,k) / U(k,k);
+  U(k+1:m,k:m) = U(k+1:m,k:m) - L(k+1:m,k) * U(k,k:m);
+end
