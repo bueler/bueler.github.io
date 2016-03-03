aab50d757f5a42caabd14106dda3a47c1d2a5251
Ed Bueler <elbueler@alaska.edu>
2016-03-02 18:12:18 -0900

    tidy up movie and post

--- M422S16/matlab/ellipsemovie.m
+++ M422S16/matlab/ellipsemovie.m
@@ -1,50 +1,51 @@
 function ellipsemovie
-% ELLIPSEMOVIE fixme
+% ELLIPSEMOVIE Visualize function
+%               1
+%   f(z) = z + ---
+%               z
+% by a movie of how it transforms circles centered at the origin (z-plane)
+% into ellipses with foci +-2 (w-plane).  Shows unit disc in z-plane (pink)
+% and foci of ellipses in w-plane.  Compare Exercises 6 and 7 in section II.6
+% of Gamelin.
 
 L = 4.1;
 
 % parameterize circle
 theta = linspace(0,2*pi,201);
 
-figure(1)
-clf
-
 % set up z-plane
-subplot(1,2,1)
-hold on
-drawaxes(L)
-fill(cos(theta),sin(theta),[1 0.4 0.4])
+figure(1),  clf
+subplot(1,2,1),  hold on
+fill(cos(theta),sin(theta),[1 0.75 0.75],'linewidth',0)  % unit disc
 text(-0.9*L,0.9*L,'z','fontsize',16)
-axis equal, axis tight, axis off
-hold off
-% set up w-plane
-subplot(1,2,2)
-hold on
-drawaxes(L)
-text(0.1*L,0.9*L,'w = z + 1/z','fontsize',16)
-axis equal, axis tight, axis off
-hold off
+drawaxes(L),  hold off
 
-% initial draw
+% set up w-plane
+subplot(1,2,2),  hold on
+text(0.1*L,0.9*L,'w = z + 1/z','fontsize',16)
+drawaxes(L),  hold off
+
+% initial draw circle in z-plane and its image in w-plane
 r = 0.04;
 x = r * cos(theta);
 y = r * sin(theta);
-subplot(1,2,1), hold on
+subplot(1,2,1),  hold on
 hz = plot(x,y,'k','linewidth',2.0);
 A = 1.0 + 1.0/r^2;
 B = 1.0 - 1.0/r^2;
-subplot(1,2,2), hold on
+subplot(1,2,2),  hold on
 hw = plot(A*x,B*y,'k','linewidth',2.0);
+plot([-2 2],[0 0],'k.','markersize',10.0)  % foci of ellipses
 
-% redraws
+% redraw at successive radii, for a movie
 while true
     for r = [0.24:0.02:1 1.05:.05:2 2.1:.1:4]
-        % draw circle in z-plane
+        % circle in z-plane
         x = r * cos(theta);
         y = r * sin(theta);
         set(hz, 'XData', x)
         set(hz, 'YData', y)
-        % draw image in w-plane
+        % image in w-plane
         A = 1.0 + 1.0/r^2;
         B = 1.0 - 1.0/r^2;
         set(hw, 'XData', A*x)
@@ -53,10 +54,11 @@ while true
     end
 end
 
-end
+end  % function ellipsemovie
 
     function drawaxes(L)
     plot([-L L],[0 0],'k','linewidth',1.0)
     plot([0 0],[-L L],'k','linewidth',1.0)
+    axis equal, axis tight, axis off
     end
 
