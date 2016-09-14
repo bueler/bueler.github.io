8809c3420e322e17221ce171213400e340bc7c56
Ed Bueler <elbueler@alaska.edu>
2016-09-14 10:23:21 -0800

    clean up fit commetnts etc

--- M661F16/matlab/fit.m
+++ M661F16/matlab/fit.m
@@ -2,10 +2,11 @@
 % Use mostly by-hand analysis of minimizing 3-variable quadratic function by
 % computing the gradient (df) and solving a linear system (df(c) = 0).
 
+% the data
 x = [0.000 0.100 0.200 0.300 0.400 0.500 0.600 0.700  0.800  0.900  1.000];
 y = [4.914 3.666 2.289 1.655 1.029 0.739 0.393 0.090 -0.197 -0.721 -0.971];
 
-% generate gradient  df(c) = A c - b
+% generate gradient  df(c) = A c - b  from symmetric matrix A and vector b
 ex = exp(-5.0*x);
 A = zeros(3,3);
 A(1,1) = 11.0;
@@ -20,9 +21,7 @@ b = [sum(y), sum(x.*y), sum(ex.*y)]';
 c = A \ b
 
 % plot result, with original data
-plot(x,y,'ko','markersize',10)
-hold on
-xfine = 0:.01:1;         % to plot smooth curve, use many x-values
+plot(x,y,'ko','markersize',10),  hold on
+xfine = 0:.01:1;                  % to plot smooth curve, use many x-values
 plot(xfine,c(1) + c(2)*xfine + c(3)*exp(-5.0*xfine),'k')
-hold off
-xlabel x, ylabel y
+hold off,  xlabel x,  ylabel y
