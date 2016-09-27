3fb3c38fcf3f839af9a28ea0bde87ab22a1faaed
Ed Bueler <elbueler@alaska.edu>
2016-09-27 13:09:10 -0800

    fix bug in rosenbrock code:  3 --> 2

--- M661F16/matlab/rosenbrock.m
+++ M661F16/matlab/rosenbrock.m
@@ -2,10 +2,10 @@ function [f, df, Hf] = rosenbrock(x)
 % ROSENBROCK Famous banana-contoured quartic function with unique global minimum.
 
 if length(x) ~= 2,  error('x must be length 2 vector'),  end
-f = 100*(x(2)-x(1).^2).^2 + (1-x(1)).^2;;
+f = 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
 df = [-400*x(1)*(x(2)-x(1)^2) - 2*(1-x(1));
       200*(x(2)-x(1)^2)];
-Hf = [-400*x(2)+1200*x(1)^3+2,  -400*x(1);
+Hf = [-400*x(2)+1200*x(1)^2+2,  -400*x(1);
       -400*x(1),                200];
 end
 
