169f1417bf8b37417e34b6b7b6a5c6245f971b31
Ed Bueler <elbueler@alaska.edu>
2016-10-27 23:11:33 -0800

    better printing and make it a function so as to control size more easily

--- M661F16/matlab/cablebfgs.m
+++ M661F16/matlab/cablebfgs.m
@@ -1,15 +1,19 @@
+function cablebfgs(N)
 % CABLEBFGS  Compare BFGS implementations on CABLE problem at high resolution.
 % Requires: CABLE, BFGSNAIVE, BFGS
 
-N=300;
+if nargin < 1
+    N = 200;
+end
+
 f = @(u) cable(u,N,10);
 u0 = -ones(N,1);
 
-fprintf('running efficient BFGS ...\n')
+fprintf('running more efficient BFGS ...\n')
 tic, [uk, uklist, alphaklist] = bfgsbt(u0,f,1.0e-8,1000); toc
-length(alphaklist)
+fprintf('  ... %d steps\n',length(alphaklist))
 
-fprintf('\nrunning naive BFGS ...\n')
+fprintf('running original (naive) BFGS ...\n')
 tic, [uk, uklist, alphaklist] = bfgsnaive(u0,f,1.0e-8,1000); toc
-length(alphaklist)
+fprintf('  ... %d steps\n',length(alphaklist))
 
