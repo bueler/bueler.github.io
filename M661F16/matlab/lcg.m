045be0ca8526f97e91c2f14f8b2e47fd08ca7969
Ed Bueler <elbueler@alaska.edu>
2016-10-27 21:54:07 -0800

    improve comments in LCG ... no change in function

--- M661F16/matlab/lcg.m
+++ M661F16/matlab/lcg.m
@@ -2,8 +2,9 @@ function [x, xlist] = lcg(x0,A,b,...
                           atol,maxiter)
 % LCG  The linear conjugate gradient method.  This is Algorithm 5.2 in
 % Nocedal & Wright.  Will generally only succeed if  A  is a symmetric
-% positive definite (SPD) matrix.  Suitable for approximate solution to
-% large SPD systems.
+% positive definite (SPD) matrix with cond(A) of modest size.  This code is
+% suitable for approximate solution to large SPD systems if tolerance (atol)
+% is set appropriately, as otherwise n steps are done if  A  is  n x n.
 %
 % Usage:
 %   [x, xlist] = lcg(x0,A,b,atol,maxiter)
@@ -26,10 +27,13 @@ function [x, xlist] = lcg(x0,A,b,...
 %
 % Bigish Example:
 %   >> N = 1000;  A = randn(N,N);  A = A '* A + eye(N,N);    % A is SPD
-%   >> b = randn(N,1);  x0 = zeros(N,1);
+%   >> xstar = randn(N,1);  b = A * xstar;  x0 = zeros(N,1);
 %   >> [x, xlist] = lcg(x0,A,b,1.0e-6);
-%   >> norm(A*x - b)/norm(b),  size(xlist,2)
-% (Result is o.k. ... but preconditioning needed.)
+%   >> size(xlist,2)                % number of iterates
+%   >> norm(A*x - b) / norm(b)      % properly-scaled size of residual
+%   >> norm(x - xstar)              % absolute size of error
+% The result in this example is o.k. ... but preconditioning is needed to
+% significantly reduce the number of iterates.
 
 % do checks
 n = size(A,1);
@@ -50,10 +54,10 @@ for k = 1:maxiter
     Ap = A * p;                                % only mat-vec is here
     alpha = rsqr / (p' * Ap);
     x = x + alpha * p;
-    if nargout > 1,  xlist = [xlist x];  end   % append x to list
-    r = r + alpha * Ap;
+    if nargout > 1,  xlist = [xlist x];  end   % optional: append x to list
+    r = r + alpha * Ap;                        % faster than: r = A * x - b
     rsqrnew = r' * r;
-    if sqrt(rsqrnew) < atol
+    if sqrt(rsqrnew) < atol                    % same as ||grad f(xk)|| < atol
         break;
     end
     beta = rsqrnew / rsqr;
