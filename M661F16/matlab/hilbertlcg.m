4ec6c8ac573237403294699e95fbde6a739f1c34
Ed Bueler <elbueler@alaska.edu>
2016-11-03 09:33:04 -0800

    a code for solutions to A6; will need to add link

---
+++ M661F16/matlab/hilbertlcg.m
@@ -0,0 +1,8 @@
+% HILBERTLCG  Test LCG on Hilbert matrices of various sizes.
+
+fprintf('  n:  iterations   residual norm\n')
+for n = [5 8 12 20]
+    A = hilb(n);  b = ones(n,1);  x0 = zeros(n,1);
+    [x, xlist] = lcg(x0,A,b,1.0e-6,1000);
+    fprintf(' %2d:         %3d        %.2e\n',n,length(xlist),norm(A*x-b))
+end
