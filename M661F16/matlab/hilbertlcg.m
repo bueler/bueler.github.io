584a86fe4738fd0aaeef5654c3f7fe4273e5a068
Ed Bueler <elbueler@alaska.edu>
2016-11-03 16:06:44 -0800

    slightly better

--- M661F16/matlab/hilbertlcg.m
+++ M661F16/matlab/hilbertlcg.m
@@ -4,5 +4,6 @@ fprintf('  n:  iterations   residual norm\n')
 for n = [5 8 12 20]
     A = hilb(n);  b = ones(n,1);  x0 = zeros(n,1);
     [x, xlist] = lcg(x0,A,b,1.0e-6,1000);
-    fprintf(' %2d:         %3d        %.2e\n',n,length(xlist),norm(A*x-b))
+    fprintf(' %2d:         %3d        %.2e\n',...
+            n, size(xlist,2), norm(A*x-b))
 end
