bc775c955814237cfbb4fec148caff1628aacd5a
Ed Bueler <elbueler@alaska.edu>
2016-11-09 17:33:35 -0900

    update codes; the test script was sent to Erica

--- M661F16/matlab/testrsimp.m
+++ M661F16/matlab/testrsimp.m
@@ -1,4 +1,6 @@
-% TESTRSIMP  
+% TESTRSIMP  a script to test my implementation of mu "phase II"
+% revised simplex method code (RSIMPII) based on Algorithm 13.1 in
+% in Nocedal & Wright
 
 % Example from wiki page for "revised simplex method":
 c = [-2 -3 -4 0 0]';
@@ -6,17 +8,26 @@ A = [3 2 1 1 0;
      2 5 3 0 1];
 b = [10 15]';
 BB = [4 5]';
-[x f lambda] = rsimpII(c, A, b, BB)
+% your code:
+[x f lambda] = rsimpII(c, A, b, BB);
+% black box:
 [xmin, fmin, status, extra] = glpk(c, A, b);
-xmin, fmin, extra.lambda
+norm(x - xmin)
+abs(f - fmin)
+norm(lambda - extra.lambda)
 
-% Example 13.1 in Nocedal & Wright:
+% Example 13.1 in Nocedal & Wright; I believe this results in the example are
+% loaded with errors
 c = [-4 -2 0 0]';
 A = [1 1 1 0; 2 0.5 0 1];
 b = [5 8]';
 BB = [3 4]';
-[x f lambda] = rsimpII(c, A, b, BB)
+% your code:
+[x f lambda] = rsimpII(c, A, b, BB);
+% black box:
 [xmin, fmin, status, extra] = glpk(c, A, b);
-xmin, fmin, extra.lambda
+norm(x - xmin)
+abs(f - fmin)
+norm(lambda - extra.lambda)
 
 
