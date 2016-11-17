7cc2684bed0e8f2501b98a4ea4344706465e8c50
Ed Bueler <elbueler@alaska.edu>
2016-11-16 21:27:03 -0900

    improve/document program

--- M661F16/matlab/p20cd.m
+++ M661F16/matlab/p20cd.m
@@ -1,14 +1,27 @@
-% P20CD solution to a problem
+% P20CD part of solution to P20
 
-J = [2, 2; exp(2), -1];
-r = [1; 0.5*exp(2)-1];
-p = J \ (-r);
-phi = @(alpha) 0.5 * ( (1+p(1)*alpha).^2 + (1+p(2)*alpha).^2 - 1 ).^2 ...
-             + 0.5 * ( 0.5 * exp(2*(1+p(1)*alpha)) - (1 + p(2)*alpha) ).^2;
-alf = 0:.01:8;
-plot(alf,phi(alf))
-hold on
-plot(1.0,phi(1.0),'ro')
-hold off
-grid on, xlabel('\alpha')
+% set up functions
+JJ = @(x) [2 * x(1),  2 * x(2);  exp(2 * x(1)),  -1];
+rr = @(x) [x(1)^2 + x(2)^2 - 1;  0.5 * exp(2 * x(1)) - x(2)];
+ff = @(x) 0.5 * rr(x)' * rr(x);       % merit function
+
+% find p for first iterate
+x0 = [1; 1];
+p0 = JJ(x0) \ (-rr(x0))
+
+% line search function at x0 in direction p0
+phi = @(alpha) ff(x0 + alpha * p0);
+
+% plot phi(alpha)
+alf = 0:.01:7;
+y = zeros(size(alf));
+for k = 1:length(alf)
+   y(k) = phi(alf(k));
+end
+plot(alf,y,'linewidth',2),  hold on
+plot(1.0,phi(1.0),'ro','markersize',14),  hold off
+grid on,  xlabel('alpha'),  ylabel('phi(alpha)')
+
+% add Wolfe condition lines to graph
+%FIXME
 
