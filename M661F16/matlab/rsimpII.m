f16017e5fc2c98423f9ed289895bc4782c093a4d
Ed Bueler <elbueler@alaska.edu>
2016-11-22 13:17:19 -0900

    document and clean up; functional change makes output Inf if unbounded feasible region

--- M661F16/matlab/rsimpII.m
+++ M661F16/matlab/rsimpII.m
@@ -1,24 +1,41 @@
-function [x fval lambda] = rsimpII(c, A, b, BB0)
+function [x f lambda k] = rsimpII(c, A, b, BB)
 % RSIMPII  Phase II of reduced simplex method.  Contains Procedure 13.1
-% from Nocedal & Wright.
+% from Nocedal & Wright.  Requires linear program in standard form
+%     min c'*x   subject to   A x = b,  x >= 0
+% and (phase II) the initial basis BB as an index set on columns of A.
+%
+% Usage:
+%     [x f lambda k] = rsimpII(c, A, b, BB)
+% where
+%     x      = solution; will be infinite (nonfeasible) if no solution
+%              because feasible set is unbounded
+%     f      = value c'*x at solution
+%     lambda = lagrange multipliers (dual variables) at solution
+%     c      = column vector of length n
+%     A      = m by n matrix
+%     b      = column vector of length m
+%     BB     = initial basis indices; must be subset of {1,...,n} with
+%              with size m and so that corresponding columns of A are
+%              linearly-independent:  B = A(:,BB) is invertible
 
 % input checking: vectors
-c = c(:);  % force into columns
-b = b(:);
+c = c(:);  b = b(:);  % force into columns
 n = length(c);
 m = length(b);
 if any(size(A) ~= [m n]),  error('A must be m by n'),  end
 
 % input checking: indices
-BB = BB0(:);
+BB = BB(:)';  % force row
 if length(BB) ~= m,  error('BB must contain m indices'),  end
 if ~isindex(BB),  error('BB must be positive integers'), end
 if any(BB > n),  error('indices in BB must be <= n'),  end
-if cond(A(:,BB)) > 1.0e10,  warning('columns of A indicated by BB may not be linearly independent'),  end
+if cond(A(:,BB)) > 1.0e10
+    warning('columns of A indicated by BB may not be linearly independent')
+end
 NN = setdiff((1:n)',BB);   % initial N indices are ordered
 
 % repeat Procedure 13.1; there exist examples where 2^n steps needed
-for k = 1:2^n
+for k = 0:2^n
     % new primal (x) and dual (lambda) values
     x = zeros(n,1);
 	x(BB) = A(:,BB) \ b;
@@ -33,7 +50,8 @@ for k = 1:2^n
     % the step
     d = A(:,BB) \ A(:,NN(q));
     if all(d <= 0)
-        error('problem is unbounded')
+        x = inf(n,1);   % invalidate x (nonfeasible) because unbounded
+        break
     end
     % the ratio test;  pindex is leaving index
     BDPOS = BB(d>0);
@@ -43,5 +61,5 @@ for k = 1:2^n
     BB(BB == pindex) = NN(q);
     NN(q) = pindex;
 end
-fval = c' * x;
+f = c' * x;
 
