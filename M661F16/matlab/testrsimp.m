68d36890b502953c04ab35ba53b51478b1067cf4
Ed Bueler <elbueler@alaska.edu>
2016-11-22 13:17:59 -0900

    add test cases: unbounded (E=4,5) and Klee-Minty cube in 4D (E=6)

--- M661F16/matlab/testrsimp.m
+++ M661F16/matlab/testrsimp.m
@@ -3,22 +3,22 @@
 % Compares against the octave linear programming black box GLPK; see
 %   https://www.gnu.org/software/octave/doc/v4.0.0/Linear-Programming.html
 
-for K = 1:3
-    switch K
+for E = 1:6
+    switch E
         case 1
             % Example 13.1 in Nocedal & Wright; book version has errors
             c = [-4 -2 0 0]';
             A = [1   1 1 0;
                  2 0.5 0 1];
             b = [5 8]';
-            BB = [3 4]';
+            BB = [3 4];
         case 2
             % Example from wiki page for "revised simplex method":
             c = [-2 -3 -4 0 0]';
             A = [3 2 1 1 0;
                  2 5 3 0 1];
             b = [10 15]';
-            BB = [4 5]';
+            BB = [4 5];
             % by hand:  x = [0 0 5 5 0]', lambda = [0 -4/3]'
         case 3
             % Example 5.2 on page 132 of Griva, Nash, and Sofer
@@ -27,16 +27,52 @@ for K = 1:3
                  -1 2 0 1 0;
                   1 0 0 0 1];
             b = [2 7 3]';
-            BB = [3 4 5]';
+            BB = [3 4 5];
             % by hand:  x = [3 5 3 0 0]', lambda = [0 -1 -2]'
+        case 4
+            % Example 5.3 on page 134 of Griva, Nash, and Sofer; unbounded
+            c = [-1 -2 0 0]';
+            A = [-1 1 1 0;
+                 -2 1 0 1];
+            b = [2 1]';
+            BB = [3 4];
+        case 5
+            % unbounded but origin is solution so succeeds
+            c = [1 2 0 0]';
+            A = [-1 1 1 0;
+                 -2 1 0 1];
+            b = [2 1]';
+            BB = [3 4];
+        case 6
+            % Example from wiki page "Klee-Minty cube", in D=4 dimensions
+            c = [-8 -4 -2 -1 0 0 0 0]';
+            A = [ 1 0 0 0 1 0 0 0;
+                  4 1 0 0 0 1 0 0;
+                  8 4 1 0 0 0 1 0;
+                 16 8 4 1 0 0 0 1];
+            b = [5, 25, 125, 625]';
+            BB = [5 6 7 8];
         otherwise
             error('invalid example case')
     end  % switch
+
     % run rsimpII:
-    [x, f, lambda] = rsimpII(c, A, b, BB);
+    [x, f, lambda, k] = rsimpII(c, A, b, BB);
     % run black box:
     [xgl, fgl, status, extra] = glpk(c, A, b);
-    fprintf('    case %d:  x within %.2e,  lambda within %.2e\n',...
-            K, norm(x - xgl), norm(lambda - extra.lambda))
+
+    % describe results
+    fprintf('  case %d:  ',E)
+    if all(isfinite(x)) & (status == 0)
+        fprintf('%2d steps,  x within %.2e,  lambda within %.2e\n',...
+                k, norm(x - xgl), norm(lambda - extra.lambda))
+    else
+        if status == 0
+            fprintf('ERROR: RSIMPII reports unbounded but GLPK reports success!\n',status)
+        else
+            fprintf('RSIMPII reports unbounded; GLPK reports status = %d\n',status)
+        end
+    end
+    fflush(stdout);
 end
 
