8fd8eaf70dadedc6500297568a3ee39c26c59f1d
Ed Bueler <elbueler@alaska.edu>
2017-02-08 09:25:37 -0900

    correct error in this code coming from post-run edits; this version runs

--- M615S17/matlab/fdrates.m
+++ M615S17/matlab/fdrates.m
@@ -6,9 +6,9 @@ x = 1.0;   duexact = cos(x);
 Dplus    = (u(x+h) - u(x)) ./ h;
 D0       = (u(x+h) - u(x-h)) ./ (2.0 * h);
 D3       = (2.0 * u(x+h) + 3.0 * u(x) - 6.0 * u(x-h) + u(x-2.0*h)) ./ (6.0 * h);
-loglog(h,abs(Dpluserr - duexact),'k.-','markersize',18,
-       h,abs(D0err - duexact),'k.-','markersize',18,
-       h,abs(D3err - duexact),'k.-','markersize',18)
+loglog(h,abs(Dplus - duexact),'k.-','markersize',18,
+       h,abs(D0 - duexact),'k.-','markersize',18,
+       h,abs(D3 - duexact),'k.-','markersize',18)
 text(0.0015,2.0e-3,'D_+','fontsize',20)
 text(0.0015,1.0e-6,'D_0','fontsize',20)
 text(0.0015,1.0e-9,'D_3','fontsize',20)
