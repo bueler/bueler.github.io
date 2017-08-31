c6537c91286c062563d8714099b62375e198b469
Ed Bueler <elbueler@alaska.edu>
2017-08-31 13:50:16 -0800

    better mycostest.m, esp. after testing with Matlab

--- M310F17/matlab/mycostest.m
+++ M310F17/matlab/mycostest.m
@@ -1,14 +1,18 @@
 function mycostest(tol)
-% MYCOSTEST  Compare built-in COS with MYCOS.  Shows 1000 cases, "." for
-% within tolerance and "X" for not.
+% MYCOSTEST  Compare built-in COS with MYCOS.  Shows 500 cases, "." for
+% within tolerance, i.e. |mycos(x)-cos(x)|<tol, and "X" for not.
+% Example:
+%   >> mycostest(1.0e-6)
+%   >> mycostest(1.0e-8)
 
-for j=1:1000
-    x = 100*randn();
-    if abs(cos(x)-mycos(x)) > tol
-        printf('X')
-    else
-        printf('.')
+for k = 1:10
+    for j = 1:50
+        x = 100*randn();
+        if abs(cos(x)-mycos(x)) > tol
+            fprintf('X')
+        else
+            fprintf('.')
+        end
     end
+    fprintf('\n')
 end
-printf('\n')
-
