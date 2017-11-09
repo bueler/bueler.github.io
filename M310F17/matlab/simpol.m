979af12f57e0e1b7f1a86f1d4aa7acb2aafdeafd
Ed Bueler <elbueler@alaska.edu>
2017-11-09 14:20:53 -0900

    re-repost: the fix was unnecessary

--- M310F17/matlab/simpol.m
+++ M310F17/matlab/simpol.m
@@ -7,10 +7,6 @@ if n <= 0, error('n must be positive'), end
 if mod(n,2) ~= 0, error('n must be even'), end
 h = (b - a) / n;
 x = a:h:b;
-if n == 2
-    c = [1, 4, 1];
-else
-    c = [1, repmat([4 2],1,(n-2)/2), 4, 1];   % the coefficients
-end
+c = [1, repmat([4 2],1,(n-2)/2), 4, 1];   % the coefficients
 z = (h / 3) * sum(c .* f(x));
 
