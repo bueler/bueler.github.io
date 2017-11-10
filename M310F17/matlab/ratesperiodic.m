0dc3af1dcc06557d7805949c04d03ef9ad782de0
Ed Bueler <elbueler@alaska.edu>
2017-11-09 21:02:43 -0900

    post a code from solutions to A8

---
+++ M310F17/matlab/ratesperiodic.m
@@ -0,0 +1,17 @@
+% RATESPERIODIC   Compare convergence rates for trapezoid rule in cases
+% where the function is, and is not, periodic.
+
+fper = @(x) exp(sin(pi * x));       exactper = 2.53213175550403;
+fnot = @(x) exp(-x.^2);             exactnot = erf(2) * sqrt(pi) / 2;
+
+nlist = [1 2 4 8 16 32];
+hlist = 2 ./ nlist;
+for k = 1:6
+    errper(k) = abs(trap(fper,0,2,nlist(k)) - exactper);
+    errnot(k) = abs(trap(fnot,0,2,nlist(k)) - exactnot);
+end
+loglog(hlist,errper,'ko',hlist,errnot,'k*')
+legend('periodic integrand', 'non-periodic integrand','location','southeast')
+ylabel('absolute error'),  xlabel h
+set(gca(),'xtick',hlist),  grid on,  axis tight
+
