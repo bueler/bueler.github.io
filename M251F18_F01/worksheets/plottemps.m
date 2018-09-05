90e30b72ff658cc47b907758dbae979da047495f
Ed Bueler <elbueler@alaska.edu>
2018-09-04 20:21:26 -0800

    making progress

--- M251F18_F01/worksheets/plottemps.m
+++ M251F18_F01/worksheets/plottemps.m
@@ -6,16 +6,17 @@ year = data(:,1);
 temp = data(:,2);
 
 figure(1)
-plot(year,temp,'o-')
+plot(year,temp,'ko-')
 xlabel('year','fontsize',24.0)
-ylabel('temperature anomaly','fontsize',24.0)
+ylabel('temperature anomaly (C)','fontsize',24.0)
 title('global temperature relative to 1951-1980 average','fontsize',30.0)
 
 year_recent = year(end-27:end);
 temp_recent = temp(end-27:end);
 
 figure(2)
-plot(year_recent,temp_recent,'o-')
+plot(year_recent,temp_recent,'ko-')
 xlabel('year','fontsize',24.0)
-ylabel('temperature anomaly','fontsize',24.0)
+ylabel('temperature anomaly (C)','fontsize',24.0)
+grid on
 
