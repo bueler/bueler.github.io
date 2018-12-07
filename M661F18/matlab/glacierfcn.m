6b8a72d3a2c68711f83ed9b5ae798a9959497e37
Ed Bueler <elbueler@alaska.edu>
2018-12-06 16:21:54 -0900

    I think this is the correct Hessian, but this stuff is not tested yet

--- M661F18/matlab/glacierfcn.m
+++ M661F18/matlab/glacierfcn.m
@@ -21,7 +21,7 @@ for j = 1:n+1
     xmid = 0.5 * (x(j) + x(j+1));
     dudx = (u(j+1) - u(j))/dx;
     uav = 0.5 * (u(j) + u(j+1));
-    f = f + (mu / 4) * dudx^4 - m(xmid) * uav;
+    f = f + (mu / 4) * dudx^4 - mclimate(xmid) * uav;
 end
 f = f * dx;
 
@@ -33,10 +33,26 @@ for k = 1:n
     dudxr = (u(j+1) - u(j))/dx;
     xmidl = 0.5 * (x(j-1) + x(j));
     xmidr = 0.5 * (x(j) + x(j+1));
-    df(k) = - mu * (dudxr^3 - dudxl^3) - 0.5 * (m(xmidl) + m(xmidr));
+    df(k) = - mu * (dudxr^3 - dudxl^3) ...
+            - 0.5 * (mclimate(xmidl) + mclimate(xmidr));
 end
 
-Hf = 0;
+% gradient grad f(u)
+Hf = zeros(n,n);
+C = 3.0 * mu / dx;
+for k = 1:n
+    j = k+1;
+    dudxl = (u(j) - u(j-1))/dx;
+    dudxr = (u(j+1) - u(j))/dx;
+    Hf(k,k) = C * (dudxl^2 + dudxr^2);
+    if k > 1
+        Hf(k,k-1) = - C * dudxl^2;
+    end
+    if k < n
+        Hf(k,k+1) = - C * dudxr^2;
+    end
+end
+
 end % function glacierfcn
 
     function m = mclimate(x)
