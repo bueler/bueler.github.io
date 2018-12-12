84313bba71af0c5602a451e5cb7a8a4647b1d1e2
Ed Bueler <elbueler@alaska.edu>
2018-12-11 15:17:59 -0900

    factor out, and plot, exact solution

--- M661F18/matlab/obstacle.m
+++ M661F18/matlab/obstacle.m
@@ -8,7 +8,9 @@ if nargin < 1,  n = 20;         end
 if nargin < 2,  tol = 1.0e-12;  end
 
 % solve
-x0 = 0.1 * ones(n,1);  % strictly feasible
+%x0 = 0.1 * ones(n,1);  % strictly feasible
+dx = 1/(n+1);
+x0 = uexact(dx:dx:1-dx) + 1;
 [xk,lamk,xklist,lamklist] = popdip(x0,@obstaclefcn,tol);
 fprintf('%d iterations\n',size(xklist,2))
 
@@ -28,11 +30,14 @@ subplot(1,2,2)
 plot(x,xk,'r')
 hold on
 xf = 0:.001:1;
-a = 0.224437973369461;  % solve  sin(2 pi a) = 0.7 (2 pi a)
-C2 = (1/(2*pi)^2) * cos(pi*(1+2*a)) - (0.7/2) * (a + 0.5) * (a - 0.5);
-uexact = -(1/(2*pi)^2) * cos(2*pi*xf) + (0.7/2) * xf .* (xf - 1) + C2;
-uexact(abs(xf-0.5) >= a) = 0;
-uexact = 100 * uexact;
-plot(xf,uexact,'b')
+plot(xf,uexact(xf),'b')
 xlabel x, grid on
+end % function
 
+    function uu = uexact(x)
+    a = 0.224437973369461;  % solve  sin(2 pi a) = 0.7 (2 pi a)
+    C2 = (1/(2*pi)^2) * cos(pi*(1+2*a)) - (0.7/2) * (a + 0.5) * (a - 0.5);
+    uu = -(1/(2*pi)^2) * cos(2*pi*x) + (0.7/2) * x .* (x - 1) + C2;
+    uu(abs(x-0.5) >= a) = 0;
+    uu = 100 * uu;
+    end % function
