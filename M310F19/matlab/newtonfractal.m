a4e21f0de84b496b0e5254d7421995119c6b281b
Ed Bueler <elbueler@alaska.edu>
2019-09-19 10:52:36 -0800

    improve newtonfractal.m and post results

--- M310F19/matlab/newtonfractal.m
+++ M310F19/matlab/newtonfractal.m
@@ -1,41 +1,49 @@
 % NEWTONFRACTAL  Produce a figure like Figure 4.16 in Greenbaum&Chartier, but
-% using intersection of two curves in plane:
+% using intersection of two curves in the plane:
 %   x^3 - 3 x y^2 + 1 = 0
 %   3 x^2 y - y^3     = 0
+% (It is the same figure, but without using complex numbers.)
 
 N = 101;
 
+% define the functions
 f1 = @(x,y) x.^3 - 3*x.*y.^2 + 1;
 f2 = @(x,y) 3*x.^2.*y - y.^3;
+
+% define the Jacobian (this is not the most efficient way)
 df11 = @(x,y) 3*x.^2 - 3*y.^2;
 df12 = @(x,y) - 6*x.*y;
 df21 = @(x,y) 6*x.*y;
 df22 = @(x,y) 3*x.^2 - 3*y.^2;
 
+% the exact solutions are known:
 xstar = [-1.0,0.5,0.5];
 ystar = [0.0,sqrt(3)/2,-sqrt(3)/2];
 colorstar = {'g.','r.','b.'};
 
-xrange = linspace(-2,2,N);  % generate NxN grid
-
-figure(1)
-clf()
+% generate NxN grid
+xrange = linspace(-2,2,N);
 [xx,yy] = meshgrid(xrange,xrange);
+
+% first figure shows the intersecting curves and the roots
+figure(1),  clf()
 contour(xx,yy,f1(xx,yy),'c',[0 0])
 hold on
 contour(xx,yy,f2(xx,yy),'m',[0 0])
 plot(xstar(1),ystar(1),'g.','markersize',28)
 plot(xstar(2),ystar(2),'r.','markersize',28)
 plot(xstar(3),ystar(3),'b.','markersize',28)
+hold off
 axis equal
 xlabel x,  ylabel y
-title('RGB dots mark intersections of curves f_i(x,y)=0')
+title('RGB dots mark intersections of curves')
 
-% zz(k,l) is 0,1,2,3:
-%   0 if root not identified
-%   1 if (-1,0)
-%   2 if (1/2,sqrt(3)/2)
-%   3 if (1/2,-sqrt(3)/2)
+% go through the grid identifying which root is being converged to
+% by the following scheme:   zz(k,l) is 0,1,2,3 if
+%   0: root not identified
+%   1: (-1,0)
+%   2: (1/2,sqrt(3)/2)
+%   3: (1/2,-sqrt(3)/2)
 zz = zeros(size(xx));
 for k = 1:size(xx,1)
     for l = 1:size(xx,2)
@@ -56,12 +64,13 @@ for k = 1:size(xx,1)
     end
 end
 
-figure(2)
-clf()
+% second figure shows the fractal
+figure(2),  clf()
 surf(xx,yy,zz)
 colormap([1 1 1; 0 1 0; 1 0 0; 0 0 1])  % [white,green,red,blue]
 shading flat
 view(2)
 axis equal, axis off
 xlabel x,  ylabel y
+title('RGB colors show destination of the Newton iteration')
 
