933bbb4fef1216227ab021c7c706e9418b9c2a32
Ed Bueler <elbueler@alaska.edu>
2019-11-22 12:11:56 -0900

    simplify this code

--- M310F19/matlab/mytrap.m
+++ M310F19/matlab/mytrap.m
@@ -1,12 +1,6 @@
 function z = mytrap(f,a,b,n);
-% MYTRAP  Basic implementation of trapezoid rule.
-% Example:
-%   >> f = @(x) exp(-x.^2);
-%   >> mytrap(f,0,2,20)
-%   >> quad(f,0,2)          % essentially exact
+% MYTRAP  Basic implementation of trapezoid rule.  Called by MYROMBERG.
 
-h = (b-a) / n;
-x = a:h:b;
+h = (b-a)/n;  x = a:h:b;
 c = [1 2*ones(1,n-1) 1];
 z = (h/2) * sum(c .* f(x));
-
