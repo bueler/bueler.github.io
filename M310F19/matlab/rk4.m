449ede6090e1f4ddb5e7d44c0c10052b6245c7a3
Ed Bueler <elbueler@alaska.edu>
2019-12-03 14:02:23 -0900

    repost rk4.m ... now its sizes match ode45 ... irritating

--- M310F19/matlab/rk4.m
+++ M310F19/matlab/rk4.m
@@ -2,7 +2,7 @@ function [tt,yy] = rk4(f,trange,y0,n)
 % RK4  Basic implementation of the classical fourth-order Runge-Kutta
 % method.  Takes n equally-spaced steps of length h to solve
 %   y' = f(t,y),  y(t0)=y0
-% where y is a column vector, trange = [t0,t1], and h = (t1-t0)/n.
+% where y0 is a column vector, trange = [t0,t1], and h = (t1-t0)/n.
 % Compare ODE45, which uses a pair of Runge-Kutta methods and can do
 % adaptive time-stepping.
 % Example 1:  solve  y'=-2y, y(0)=1  on [0,2]
@@ -20,17 +20,17 @@ function [tt,yy] = rk4(f,trange,y0,n)
 
 if n <= 0,  error('number of steps must be positive');  end
 h = (trange(2)-trange(1))/n;
-tt = trange(1):h:trange(2);
-yy = zeros(length(y0),length(tt));
-yy(:,1) = y0(:);
+tt = (trange(1):h:trange(2))';
+yy = zeros(length(tt),length(y0));  % size follows ode45()
+yy(1,:) = y0(:)';
 
 % fourth-order Runge-Kutta from page 266 of Greenbaum & Chartier (2012)
 for k = 1:length(tt)-1
     t = tt(k);
-    q1 = f(t,yy(:,k));
-    q2 = f(t+h/2,yy(:,k)+(h/2)*q1);
-    q3 = f(t+h/2,yy(:,k)+(h/2)*q2);
-    q4 = f(t+h,yy(:,k)+h*q3);
-    yy(:,k+1) = yy(:,k) + (h/6) * (q1 + 2*q2 + 2*q3 + q4);
+    q1 = f(t,yy(k,:)');
+    q2 = f(t+h/2,yy(k,:)'+(h/2)*q1);
+    q3 = f(t+h/2,yy(k,:)'+(h/2)*q2);
+    q4 = f(t+h,yy(k,:)'+h*q3);
+    yy(k+1,:) = yy(k,:) + (h/6) * (q1 + 2*q2 + 2*q3 + q4)';
 end
 
