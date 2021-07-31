20d1b0900882c4ad64dac50f9aee5da622416f9a
Ed Bueler <elbueler@alaska.edu>
2021-07-30 17:17:51 -0800

    Maintenance on the Matlab/Octave/Python comparison doc.

--- bis.py
+++ bis.py
@@ -1,5 +1,5 @@
 def bis(a,b,f):
-    """ BIS  Apply the bisection method to solve
+    '''BIS  Apply the bisection method to solve
         f(x) = 0
     with initial bracket [a,b].  Example:
 
@@ -8,13 +8,13 @@ def bis(a,b,f):
         from math import cos
         return cos(x) - x
     r = bis(0.0,1.0,f)            
-    print([r,f(r)])                   """
+    print([r,f(r)])'''
 
     if f(a) * f(b) > 0.0:
         print("not a bracket!")
         return
     for k in range(100):
-        c = (a+b)/2
+        c = (a + b)/2
         r = f(c)
         if abs(r) < 1e-12: 
             return c # we are done
--- gaussint.py
+++ gaussint.py
@@ -1,32 +1,31 @@
-#!/usr/bin/env python
+'''plot the integrand and approximate
+the integral 
+    / 1
+    |    exp(-x^2/pi) dx
+    / 0
+by left-hand, right-hand, and
+trapezoid rules'''
 
-# plot the integrand and approximate
-# the integral 
-#    / 1
-#    |    exp(-x^2/pi) dx
-#    / 0
-# by left-hand, right-hand, and
-# trapezoid rules
-
-from pylab import plot,axis,linspace,sum, \
-                  pi,sqrt,exp,show,grid
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.special import erf
 
 N = 1000
 dx = (1.0 - 0.0) / N
-x = linspace(0.0,1.0,N+1)
-y = exp(- x**2 / pi)
+x = np.linspace(0.0,1.0,N+1)
+y = np.exp(- x**2 / np.pi)
 
-plot(x,y)
-axis([0.0,1.0,0.0,1.0]); grid(True)
+plt.plot(x,y)
+plt.axis([0.0,1.0,0.0,1.0])
+plt.grid(True)
 
 lhand = dx * sum(y[:-1])
-print "lhand = %.15f" % lhand
+print("lhand = %.15f" % lhand)
 rhand = dx * sum(y[1:])
-print "rhand = %.15f" % rhand
+print("rhand = %.15f" % rhand)
 trap  = (dx/2) * sum(y[:-1]+y[1:])
-print "trap = %.15f" % trap
-exact = (pi/2) * erf(1/sqrt(pi))
-print "exact = %.15f" % exact
-show()
+print("trap = %.15f" % trap)
+exact = (np.pi/2) * erf(1/np.sqrt(np.pi))
+print("exact = %.15f" % exact)
+plt.show()
 
