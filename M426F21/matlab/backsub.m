bbd3f9a500cfa919767a729c4e1706ee13721e34
Ed Bueler <elbueler@alaska.edu>
2021-09-16 12:22:56 -0800

    Save codes locally, and correct dumb index range aspects of forwardsub.m, backsub.m.

---
+++ M426F21/matlab/backsub.m
@@ -0,0 +1,15 @@
+function x = backsub(U,b)
+% BACKSUB   Solve an upper triangular linear system.
+% (Corrected by ELB to work in Octave.)
+% Input:
+%   U    upper triangular square matrix (n by n)
+%   b    right-hand side vector (n by 1)   
+% Output:
+%   x    solution of Ux=b (n by 1 vector)
+
+n = length(U);
+x = zeros(n,1);
+x(n) = b(n) / U(n,n);
+for i = n-1:-1:1
+  x(i) = ( b(i) - U(i,i+1:n) * x(i+1:n) ) / U(i,i);
+end
