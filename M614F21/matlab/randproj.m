8d37f7dc6add3f94725ba08ead44a4e176dd616b
Ed Bueler <elbueler@alaska.edu>
2021-10-11 14:25:21 -0800

    Post codes from A5 solutions, and update topics.

---
+++ M614F21/matlab/randproj.m
@@ -0,0 +1,8 @@
+function P = randproj(m,k)
+% RANDPROJ Generate a random m x m projector with rank k.
+% Example:
+%   >> P = randproj(7,4)       % a 7 x 7 matrix with rank 4
+
+[Q,S,V] = svd(randn(m,m));     % using qr() is a bit faster
+Qhat = Q(:,1:k);               % first k columns are ON set
+P = Qhat * Qhat';
